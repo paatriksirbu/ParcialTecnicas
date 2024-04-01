fa756155df29b5177fcf1b66cc53dded3c66b047
Patrik <psirb@myuax.com>
2024-04-02 01:44:07 +0200

    Frigate, barco de 3 posiciones del tablero.

--- src/main/java/Ejercicio1/Barco/TiposDeBarcos/Frigate.java
+++ src/main/java/Ejercicio1/Barco/TiposDeBarcos/Frigate.java
@@ -1,6 +1,19 @@
 package Ejercicio1.Barco.TiposDeBarcos;
 
 import Ejercicio1.Barco.Barco;
+import Ejercicio1.Barco.Clima;
+import Ejercicio1.Cuadricula.CuadriculaIA;
+import Ejercicio1.EstrategiaPosicionamiento.EstrategiaPosicionamiento;
 
 public class Frigate extends Barco {
+
+    public Frigate(int tamano, CuadriculaIA cuadriculaIA, int filaInicial, int columnaInicial, Clima clima, EstrategiaPosicionamiento estrategia) {
+        super(3, cuadriculaIA, filaInicial, columnaInicial, clima, estrategia);
+    }
+
+    @Override
+    public boolean disparar(int fila, int columna){
+        return super.disparar(fila, columna);
+    }
+
 }
