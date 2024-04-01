85a46379bb4beef257ecbbf03e747692aa914851
Patrik <psirb@myuax.com>
2024-04-02 01:57:17 +0200

    Canoe, barco de 1 posicion del tablero.

--- src/main/java/Ejercicio1/Barco/TiposDeBarcos/Canoe.java
+++ src/main/java/Ejercicio1/Barco/TiposDeBarcos/Canoe.java
@@ -1,6 +1,16 @@
 package Ejercicio1.Barco.TiposDeBarcos;
 
 import Ejercicio1.Barco.Barco;
-
+import Ejercicio1.Barco.Clima;
+import Ejercicio1.Cuadricula.CuadriculaIA;
+import Ejercicio1.EstrategiaPosicionamiento.EstrategiaPosicionamiento;
 public class Canoe extends Barco {
+    public Canoe(int tamano, CuadriculaIA cuadriculaIA, int filaInicial, int columnaInicial, Clima clima, EstrategiaPosicionamiento estrategia) {
+        super(1, cuadriculaIA, filaInicial, columnaInicial, clima, estrategia);
+    }
+
+    @Override
+    public boolean disparar(int fila, int columna){
+        return super.disparar(fila, columna);
+    }
 }
